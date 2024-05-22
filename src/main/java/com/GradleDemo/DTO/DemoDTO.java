16079d2d15663e0d5cbddfd92d371eefd02666be
Bhuvnesh-Rana <bhuvnesh.rana28@gmail.com>
2024-05-22 13:08:32 +0530

    Completed Entity, Dto and db settings in application.props

--- src/main/java/com/GradleDemo/DTO/DemoDTO.java
+++ src/main/java/com/GradleDemo/DTO/DemoDTO.java
@@ -1,5 +1,16 @@
 package com.GradleDemo.DTO;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class DemoDTO {
+    private Integer demoId;
+    private String demoName;
+    private String demoCity;
+    private char demoGender;
     
 }
--- src/main/java/com/GradleDemo/Entity/Demo.java
+++ src/main/java/com/GradleDemo/Entity/Demo.java
@@ -1,5 +1,25 @@
 package com.GradleDemo.Entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "GradleDemo")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Demo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer demoId;
+    private String demoName;
+    private String demoCity;
+    private char demoGender;
     
 }
